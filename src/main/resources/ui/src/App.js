3874f31b36634caf7dc899f3480616aee3a85b4e
Zhenzhen <manzhzh@163.com>
2020-07-01 14:09:36 +0800

    submodule test

---
+++ src/main/resources/ui/src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import routes from "./routes";
+import withTracker from "./withTracker";
+
+//Below 2 lines for all views which imported ReactTable, BigCalendar
+import 'react-table/react-table.css';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/main.scss";
+
+export default () => (
+    <Router basename={process.env.REACT_APP_BASENAME || ""}>
+        <div>
+            {routes.map((route, index) => {
+                return (
+                    <Route
+                        key={index}
+                        path={route.path}
+                        exact={route.exact}
+                        component={withTracker(props => {
+                            return (
+                                <route.layout {...props}>
+                                    <route.component {...props} />
+                                </route.layout>
+                            );
+                        })}
+                    />
+                );
+            })}
+        </div>
+    </Router>
+);
